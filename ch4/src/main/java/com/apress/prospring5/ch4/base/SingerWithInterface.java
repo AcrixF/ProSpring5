1c2c939b7925391dd80f84b2c4164e68783dd222
neoa <acrixf@gmail.com>
2018-08-05 10:43:12 -0500

    Bean lifecycle

---
+++ ch4/src/main/java/com/apress/prospring5/ch4/base/SingerWithInterface.java
@@ -0,0 +1,63 @@
+package com.apress.prospring5.ch4.base;
+
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class SingerWithInterface implements InitializingBean {
+
+    private static final String DEFAULT_NAME = "Nitzine Esmeralda Ocampo Avila";
+
+    private String name;
+
+    private int age = Integer.MIN_VALUE;
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public void setAge( int age ) {
+        this.age = age;
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Initializing bean");
+
+        if (name == null) {
+            System.out.println("Using default name");
+            name = DEFAULT_NAME;
+        }
+        if (age == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException(
+                    "You must set the age property of any beans of type " +  Singer.class);
+        }
+    }
+
+    public String toString(){
+        return  "\tName: " +  name +  "\n\tAge: " +  age;
+    }
+
+
+    private static SingerWithInterface getBean(String beanName, ApplicationContext ctx ) {
+        try{
+            SingerWithInterface bean = (SingerWithInterface) ctx.getBean(beanName);
+            System.out.println(bean);
+            return bean;
+        } catch (BeanCreationException ex) {
+            System.out.println("An error occured in bean configuration: "
+                    + ex.getMessage());
+            return null;
+        }
+    }
+
+    public static void main( String [] args ) {
+        GenericXmlApplicationContext  ctx  = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-xml-interface.xml");
+        ctx.refresh();
+        getBean("singerOne", ctx);
+        getBean("singerTwo", ctx);
+        getBean("singerThree", ctx);
+        ctx.close();
+    }
+}
